f73e43e27510bb5ab847b93f673297fc06b4544c
Daniel <daniel.jorca@gmail.com>
2022-11-20 19:23:05 +0100

    inicio proyecto

---
+++ ProyectoFuturo/ProyectoFuturo.java
@@ -0,0 +1,32 @@
+package ProyectoFuturo;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class ProyectoFuturo {
+
+    private String valorHoras;
+    private String numModulos;
+
+
+    public ProyectoFuturo() {
+
+    }
+
+    public String getValorHoras() {
+        return valorHoras;
+    }
+
+    public String getNumModulos() {
+        return numModulos;
+    }
+
+    static String veredicto(String numModulos, String valorHoras) {
+        if (numModulos.equals(valorHoras)) {
+            System.out.print("Bien, pero una hora de estudio para cada módulo puede ser insuficiente.");
+        }else if (Integer.parseInt(numModulos) > Integer.parseInt(valorHoras)) {
+            System.out.println("Poco tiempo de estudio. Debes dedicar mas tiempo"); ;
+        }else
+        System.out.print("Ideal. Trabajas los contenidos en casa.");
+        return "";
+    }
+}
---
+++ ProyectoFuturo/ProyectoFuturoMain.java
@@ -0,0 +1,19 @@
+package ProyectoFuturo;
+
+public class ProyectoFuturoMain {
+
+
+    public static void main(String[] args) {
+
+
+        ProyectoFuturo modulo = new ProyectoFuturo();
+
+        System.out.println("Estudiante " + args[0] + " : ");
+
+        modulo.veredicto("1" , "1");
+
+
+    }
+}
+
+
